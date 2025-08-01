ad20fa5d4c1b13ea2d9ea921095b11070bc181b0
Jeet Goswami <workingpurpose125@gmail.com>
2025-08-01 08:14:30 +0530

    Update 10_Hackerrank_Arrays_DS.c

--- Week 1 Assigment Using C/10_Hackerrank_Arrays_DS.c
+++ Week 1 Assigment Using C/10_Hackerrank_Arrays_DS.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 int main() {
     int arr[] = {1, 4, 3, 2}, n = 4;
-    for(int i = n-1; i >= 0; i--) printf("%d ", arr[i]);
+    for(int i = n-1; i >= 0; i--) 
+        printf("%d ", arr[i]);
     return 0;
 }
