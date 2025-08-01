ca7b889fe6602c6f79605b1c733ca0d02bfad5d5
Jeet Goswami <workingpurpose125@gmail.com>
2025-08-01 08:15:07 +0530

    Update 1_Insert_at_end.c

--- Week 1 Assigment Using C/1_Insert_at_end.c
+++ Week 1 Assigment Using C/1_Insert_at_end.c
@@ -6,6 +6,7 @@ void insertAtEnd(int arr[], int n, int x) {
 int main() {
     int arr[100] = {1, 2, 3}, n = 3, x = 4;
     insertAtEnd(arr, n, x);
-    for(int i = 0; i <= n; i++) printf("%d ", arr[i]);
+    for(int i = 0; i <= n; i++)
+        printf("%d ", arr[i]);
     return 0;
 }
