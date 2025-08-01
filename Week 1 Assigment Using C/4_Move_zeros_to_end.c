cae8cdcb886ed4aa339d021d8faeed3c503aba87
Jeet Goswami <workingpurpose125@gmail.com>
2025-08-01 08:15:38 +0530

    Update 4_Move_zeros_to_end.c

--- Week 1 Assigment Using C/4_Move_zeros_to_end.c
+++ Week 1 Assigment Using C/4_Move_zeros_to_end.c
@@ -9,6 +9,7 @@ void moveZeros(int arr[], int n) {
 int main() {
     int arr[] = {0, 1, 0, 3, 12}, n = 5;
     moveZeros(arr, n);
-    for(int i = 0; i < n; i++) printf("%d ", arr[i]);
+    for(int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
     return 0;
 }
