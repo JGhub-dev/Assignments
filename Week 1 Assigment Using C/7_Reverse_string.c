49f35e3384943438d5a914e445c836e117726c03
Jeet Goswami <workingpurpose125@gmail.com>
2025-08-01 08:16:54 +0530

    Update 7_Reverse_string.c

--- Week 1 Assigment Using C/7_Reverse_string.c
+++ Week 1 Assigment Using C/7_Reverse_string.c
@@ -4,6 +4,7 @@
 int main() {
     char str[] = "hello";
     int n = strlen(str);
-    for(int i = n - 1; i >= 0; i--) printf("%c", str[i]);
+    for(int i = n - 1; i >= 0; i--)
+        printf("%c", str[i]);
     return 0;
 }
